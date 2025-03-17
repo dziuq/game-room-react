79f743dd3ee62edda607fc160fdbfe646d1f226a
PC\HomePC <dawid.dziuk@wp.pl>
2025-03-17 23:43:49 +0100

    init + polaczenia z lokalnym api

---
+++ Game-Room-React/src/GameCard.js
@@ -0,0 +1,16 @@
+class GameCard {
+    constructor(id, name, description, category, yearOfProduction, producer, developer, scoreId, score, reviews){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.yearOfProduction = yearOfProduction;
+        this.producer = producer;
+        this.developer = developer;
+        this.scoreId = scoreId;
+        this.score = score;
+        this.reviews = reviews;
+    }
+
+}
+
