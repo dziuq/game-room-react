e0d1f5bc01cb62158fb312a5899d46a61cfe9c40
PC\HomePC <dawid.dziuk@wp.pl>
2025-03-19 02:04:37 +0100

    dodanie neonowego stylu

---
+++ Game-Room-React/src/NeonNavbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const NeonNavbar = () => {
+    return (
+        <nav className="neon-navbar">
+            <div className="logo">Video Games</div>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Top Games</a></li>
+                <li><a href="#">Reviews</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NeonNavbar;
--- Game-Room-React/src/main.jsx
+++ Game-Room-React/src/main.jsx
@@ -3,9 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import GameList from './GameList.jsx'
+import NeonNavbar from './NeonNavbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <>
+    <NeonNavbar/>
     <GameList/>
+    </>
+    
   </StrictMode>,
 )
